2959773904738c8ec346eacde6796ca490d91299
Alvaro <alvarojosepl@gmail.com>
2017-09-27 08:53:11 +0200

    added redux

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Button } from 'react-bootstrap';
-const buttonsInstance = (
-  <Button>Click me!</Button>
-);
-ReactDOM.render(buttonsInstance, document.getElementById('app'));
+import React , { Component } from 'react'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { Button } from 'react-bootstrap'
+import reducer from './reducers'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+function configureStore(initialState){
+  const enhancer = composeWithDevTools(
+      applyMiddleware(
+          thunkMiddleware
+      )
+  )
+  return createStore(reducer, initialState, enhancer)
+}
+
+const store = configureStore({});
+
+class App extends Component {
+  render(){ 
+      return (
+          <Provider store={store}>
+              <Button>Click me!</Button>
+          </Provider>
+      );
+  }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-import App from './app/app.js';
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+import App from './app/app.js'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css'
+console.log('hello world')
+ReactDOM.render(React.createElement(App), document.getElementById('app'))
